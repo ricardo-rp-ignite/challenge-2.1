b1eccf3a4f7ddea5055682e3ecaa92b6b4d24c28
moro <ricardoromero4444@gmail.com>
2021-10-28 23:10:52 -0300

    ✨ : Created functions for data fetching

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,17 @@
 import axios from 'axios';
+import { Product, Stock } from '../types';
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
 });
+
+export const fetchStockById = (productId: number) =>
+  api.get<Stock>(`/stock/${productId}`).then(response => response.data.amount);
+
+type FetchProductResult = Omit<Product, 'amount'>
+
+export const fetchProductById = (productId: number) =>
+  api.get<FetchProductResult>(`/products/${productId}`).then(response => response.data);
+
+export const fetchAllProducts = () =>
+  api.get<Product[]>(`/products/`).then(response => response.data)
